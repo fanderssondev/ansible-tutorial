740e426247fbed4416e91c91fb2292cc9e264f99
Fredrik Andersson <fredrikanderssondev@gmail.com>
2025-02-17 16:35:39 +0100

    updated tmux script

--- tmux_session.sh
+++ tmux_session.sh
@@ -3,13 +3,14 @@
 # Session name
 SESSION="ansible"
 
-# Start a new tmux session
+# Start a new tmux session, explicitly setting the first window
 tmux new-session -d -s $SESSION -n ansible
 
 # Create additional windows
-tmux new-window -t $SESSION:1 -n rocky8a
-tmux new-window -t $SESSION:2 -n rocky8b
-tmux new-window -t $SESSION:3 -n rocky8c
+tmux new-window -t $SESSION:2 -n rocky8a
+tmux new-window -t $SESSION:3 -n rocky8b
+tmux new-window -t $SESSION:4 -n rocky8c
 
 # Attach to session
 tmux attach -t $SESSION
+
